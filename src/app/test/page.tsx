d7adbd2b7bc7722aa9bef8bb219b8731debd89b0
Daniel <kaziev30@mail.ru>
2023-08-02 01:23:03 +0600

    init folder structure

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel porro,
+      eveniet magni deserunt optio facere! Doloribus ex modi explicabo nobis
+      laudantium in quis. Accusantium optio laboriosam blanditiis! Expedita,
+      mollitia porro.
+    </p>
+  );
+}
