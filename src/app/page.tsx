be3a387c82f5c5129337bb8c3b7d13696f03f9b6
Daniel <kaziev30@mail.ru>
2023-08-05 00:39:30 +0600

    preparing folders

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <p>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias perspiciatis exercitationem ducimus perferendis molestias labore error, veniam amet quod repellendus sed, commodi quaerat earum delectus dolorem. Nostrum iste quibusdam aspernatur?
-    </p>
-  )
-}
---
+++ src/components/test.tsx
@@ -0,0 +1,11 @@
+export const Lalala = () => {
+    return <>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus laborum amet quas, fugit tenetur fugiat, eaque quos debitis nihil eos aliquid accusamus quasi veniam, illo sunt nisi rerum quia magni?</p>
+        <div>
+            <button type="button">
+                press
+            </button>
+        </div>
+    </>;
+  }
+  
